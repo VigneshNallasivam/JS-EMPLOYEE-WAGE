445c82fe4ccd8dd7ad33ca02284742d84ee3c82a
VigneshNallasivam <vickyshanestark@gmail.com>
2023-01-20 14:39:48 +0530

    Added : UC-3 Checking Employee Daily Working Hours

---
+++ DailyWorkingHours.js
@@ -0,0 +1,22 @@
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+let workingHours;
+function EmployeeWage() {
+    
+    let ramdomValue = Math.floor(Math.random() * 10) % 3;
+    switch (ramdomValue) {
+        case IS_FULL_TIME:
+            workingHours = FULL_TIME_HOURS;
+            break;
+        case IS_PART_TIME:
+            workingHours = PART_TIME_HOURS
+            break;
+        default:
+            workingHours = 0;
+    }
+    console.log("Employee Working hours : "+workingHours);
+    console.log("Employee wage per day : " + workingHours * WAGE_PER_HOUR);
+}
