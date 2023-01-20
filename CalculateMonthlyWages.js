895ebdc5fb1e20450cba171b22aa487de41f293d
VigneshNallasivam <vickyshanestark@gmail.com>
2023-01-20 14:43:48 +0530

    Added : UC-4 Calculating Employee Monthly Wages

---
+++ CalculateMonthlyWages.js
@@ -0,0 +1,29 @@
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const WORKING_DAYS_PER_MONTH = 20;
+function EmployeeWage() {
+    /**
+     * In this method calculate the Employee Wage
+     */
+    let workingHours = 0;
+    let day = 1;
+    while (day <= WORKING_DAYS_PER_MONTH) {
+        let ramdomValue = Math.floor(Math.random() * 10) % 3;
+        switch (ramdomValue) {
+            case IS_FULL_TIME:
+                workingHours += FULL_TIME_HOURS;
+                break;
+            case IS_PART_TIME:
+                workingHours += PART_TIME_HOURS
+                break;
+            default:
+                workingHours += 0;
+        }
+        day++;
+    }
+    console.log("Employee Working hours in month : " + workingHours);
+    console.log("Employee Total wage per month : " + workingHours * WAGE_PER_HOUR);
+}
